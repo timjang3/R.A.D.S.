5b82035c4744338ec6ef7d3e3056d4424535c0f5
Tim Jang <timjang3@gmail.com>
2021-04-18 13:29:50 -0700

    name change

--- index.js
+++ index.js
@@ -1,73 +1,75 @@
-const express = require('express')
-const app = express()
-const port = 5600;
+
+var path = require('path');
+var express = require('express');
+const app = express();
 
 let avails = [0, 0, 0];
-let time = [0, 0, 0];
-const {performance} = require('perf_hooks');
-let date = new Date();
 
-let weekCount = 0;
+app.use(express.json());
 
-let masterData = [];
-
-let data = [];
-
-for(i = 0; i < 7;  i++){
-  data[i] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-}
-
-let avgData = [...data];
-
-for(i = 0; i < 4; i++){
-  masterData[i] = [...data];
-}
-
-app.get('/profile/:id', function(req, res){
-  res.send('You requested to see profile with ID of ' + req.params.id);
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-app.get('/', function(req, res) {
-    var roomNumber = req.param('r');
-    var availability = req.param('f');
-
-    avails[roomNumber] = parseInt(availability);
-
-    res.send(avails);
-
-    if(parseInt(availability) == 0){
-      time[roomNumber] = performance.now();
-    }
-    else if(parseInt(availability) == 1){
-      let day = date.getDay();
-      let hour = date.getHours();
-      if(day == 0){
-        weekCount ++;
-      }
-      masterData[weekCount][day][hour] = 1;
-    }
-    
-      for(j  = 0; j < 7; j++){
-        for(y = 0; y < 24; y++){
-          avgData[j][y] = masterData[masterData.length-1][j][y] + masterData[masterData.length-2][j][y] + masterData[masterData.length-3][j][y] + masterData[masterData.length-4][j][y]; 
-        }
-      }
-
-      //console.log(avgData);
-     
+app.use((req, res, next) => {
+    console.log(req.path);
+    next();
 });
 
+app.use(express.static(path.join(__dirname, "public")));
 
-setInterval(print, 1000)
+app.get("/avails", (req, res) => res.json(avails));
 
-function print(){
-  let temp = [0, 0, 0];
-  for(i = 0; i < 3; i++){
-    temp[i] = performance.now() - time[i];
-  }
-  console.log(temp);
-}
+app.get('/test', function(req, res){
+    var room = req.query.r;
+    var status = req.query.f;
+    avails[room] = status;
+  });
+
+
+/*
+app.post("/", (req, res) => {
+    const room = req.param('r');
+    const newStatus = req.param('f');
+    avails[room] = newStatus;
+    console.log("new avails:", avails);
+    res.json({ message: "Success!", avails });
+});
+
+app.post("/rooms/:room/status", (req, res) => {
+    if (typeof req.body["value"] !== "number")
+        return res.status(400).json({ message: "No valid new value is specified" });
+    const room = +req.params["room"];
+    const newStatus = req.body["value"];
+    avails[room] = newStatus;
+    console.log("new avails:", avails);
+    res.json({ message: "Success!", avails });
+});
+*/
+
+app.listen(3000, () => console.log("Server is ready"));
+/*
+http.createServer(function (req, res) {
+    let route = req.url.replace('/', '')
+
+    let request_url = route === '' || route === '/' ? 'website.html' : route
+
+    var room = url.parse(req.url, true).query['r'];
+    var status = url.parse(req.url, true).query['f'];
+
+    avails[room] = parseInt(status);
+    console.log(avails);
+
+    console.log(request_url)
+
+    fs.exists(request_url, (exist) => {
+        if (Boolean(exist) === false) {
+            res.writeHead(404, {'content-type': 'text/html'})
+            res.end('Page Not Found')
+            return null
+        }
+
+        fs.readFile(request_url, function (err, data) {
+            res.writeHead(200)
+            res.write(data)
+            res.end()
+        })
+    })
+}).listen(8080);
+*/
